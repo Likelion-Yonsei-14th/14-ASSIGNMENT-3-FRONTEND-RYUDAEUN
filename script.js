02a6c505cd28cb99059028ff50cdc0e7281941c8
류다은 <ryudahlia@yonsei.ac.kr>
2026-04-23 20:37:42 +0900

    docs: change favourite.html to movie.html

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-const items = document.querySelectorAll(".activity");
+const activityItems = document.querySelectorAll(".activity");
 
-items.forEach((item, index) => {
+activityItems.forEach((item, index) => {
   setTimeout(() => {
     item.classList.add("show");
-  }, index * 300); // 순차적으로 등장
+  }, index * 300);
 });
+
+
+const copyItems = document.querySelectorAll(".copy-text");
+
+copyItems.forEach(item => {
+  item.addEventListener("click", () => {
+    const text = item.querySelector("p").innerText;
+
+    navigator.clipboard.writeText(text);
+
+    item.style.opacity = "0.5";
+
+    setTimeout(() => {
+      item.style.opacity = "1";
+    }, 200);
+  });
+}); 
